5e31405313686415ee17b0323393c2b9c3ee5e31
l-trochelmann <l.trochelmann@gmail.com>
2025-04-21 12:57:06 +0200

    adapt for baseline config

--- cluster/single_gpu/condor.sh
+++ cluster/single_gpu/condor.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-source ~/miniforge3/etc/profile.d/conda.sh
-conda activate plainLM
+export HOME=/home/ltrochelmann
+
+echo "Activating environment"
+source /home/ltrochelmann/miniforge3/etc/profile.d/conda.sh
+conda activate ln-variants
 
 # Job specific vars
+echo "Assigning job variables"
 config=$1
 job_idx=$2 # CONDOR job arrays range from 0 to n-1
 
 # Execute python script
-python train.py --config=$config --job_idx=$job_idx
+echo "Executing job script"
+python /home/ltrochelmann/LN-variants/train.py --config=$config --job_idx=$job_idx
