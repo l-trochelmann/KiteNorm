5e88f35c1196a573fbfa012b475424960d57ca36
l-trochelmann <l.trochelmann@gmail.com>
2025-05-26 16:56:28 +0200

    Remove redundant comment

--- train.py
+++ train.py
@@ -97,7 +97,6 @@ def main(_):
   if master_process and cfg.save_last_checkpoint:
     save_checkpoint(micro_step-1, model, engine, cfg, JOB_IDX)
 
-  print_master(f"=== Checkpoint saved! ===")
   print_master(f"=== Terminating... ===")
 
   # DDP slaughtering
