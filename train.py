06265a05e41a0af174e7f7ac8992b3b5582158d6
l-trochelmann <l.trochelmann@gmail.com>
2026-03-12 10:30:21 +0100

    update train.py

--- train.py
+++ train.py
@@ -20,11 +20,19 @@ FLAGS = flags.FLAGS
 
 
 def main(_):
+  print("=== Prepare Training... ===")
   
   CFG_PATH, JOB_IDX = FLAGS.config, FLAGS.job_idx
   cfg, _ = utils.load_config(CFG_PATH, JOB_IDX)
   
   local_rank, world_size, device, master_process = pytorch_setup(cfg)
+
+  print("Using device:", device)
+  print("torch.cuda.is_available():", torch.cuda.is_available())
+  if torch.cuda.is_available():
+      print("CUDA device count:", torch.cuda.device_count())
+      print("Current CUDA device:", torch.cuda.current_device())
+      print("GPU name:", torch.cuda.get_device_name(torch.cuda.current_device()))
   
   if master_process:
     utils.maybe_make_dir(cfg, JOB_IDX)
